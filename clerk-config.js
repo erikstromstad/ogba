024c018ec9df61bdedcfcf286bfc8930092b8e98
erikstromstad <erikstromstad@gmail.com>
2026-04-02 20:15:57 -0500

    Update clerk-config.js

--- clerk-config.js
+++ clerk-config.js
@@ -1,10 +1,10 @@
 window.OGBA_CONFIG = {
-  clerkPublishableKey: "pk_test_REPLACE_ME",
+  clerkPublishableKey: "pk_live_Y2xlcmsub2diYS5nYW1lcyQ",
   clerkJsUrl: "https://caring-bear-70.clerk.accounts.dev/npm/@clerk/clerk-js@5/dist/clerk.browser.js",
   adminAllowedUserIds: [
     "user_REPLACE_WITH_YOUR_CLERK_USER_ID"
   ],
   adminAllowedEmails: [
-    "REPLACE_WITH_ADMIN_EMAIL@example.com"
+    "erikstromstad@gmail.com"
   ]
 };
